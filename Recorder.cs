3545d61abef5b6cf9b03677a347229b214ffa765
Darren Tsung <darren.tsung@gmail.com>
2017-08-09 19:50:44 -0700

    Update to match latest DTDebugMenu API

--- Recorder.cs
+++ Recorder.cs
@@ -77,7 +77,7 @@ namespace DTMediaCapture {
 			var inspector = DTDebugMenu.GenericInspectorRegistry.Get("DTMediaCapture");
 			inspector.BeginDynamic();
 			inspector.RegisterHeader("Recorder");
-			inspector.RegisterToggle("Set Recording: " + (useKeyBindings_ ? string.Format("({0})", toggleRecordingKey_) : ""), (b) => { if (b) { StartRecording(); } else { StopRecording(); }}, () => recording_);
+			inspector.RegisterToggle("Set Recording: " + (useKeyBindings_ ? string.Format("({0})", toggleRecordingKey_) : ""), () => recording_, (b) => { if (b) { StartRecording(); } else { StopRecording(); }});
 			dynamicGroup_ = inspector.EndDynamic();
 			#endif
 		}
